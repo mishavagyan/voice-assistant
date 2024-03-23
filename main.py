0eab690e1b91aa3a3f591dd1d49d38f19f344ecd
mishavagyan <avagyanmexak@gmail.com>
2024-03-23 14:22:54 +0400

    recognizing russian speech, KeyboardInterrupt

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ while True:
 
     try:
         with sr.Microphone() as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            recognizer.adjust_for_ambient_noise(mic, duration=0.1)
             audio = recognizer.listen(mic)
-
-            text = recognizer.recognize_google(audio)
+            # for russian
+            text = recognizer.recognize_google(audio, language="ru-Ru")
             text = text.lower()
 
             print(f"Recognized text:\n{text}") 
+
+    except KeyboardInterrupt:
+        break
 
     except sr.UnknownValueError:
         recognizer = sr.Recognizer() 
